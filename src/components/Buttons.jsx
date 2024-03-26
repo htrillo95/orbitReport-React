0ee355734e398b8efa5233e79df0b522930b943c
Hector Trillo <htrillo95@gmail.com>
2024-03-25 21:29:24 -0400

    update

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import satData from './satData';
+import './styling.css';
 
 //Buttons component
 const Buttons = ({ filterByType, setSat, displaySats }) => {
   return (
 
     //Div contain the buttons
-    <div>
+    <div className="flex-container"> {/* Update className to "flex-container" */}
 
       {/* Map through displaySats creates buttons for each orbit type */}
       {displaySats.map((sat, id) => (
